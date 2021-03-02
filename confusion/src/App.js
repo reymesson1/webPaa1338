73cdf2db2405f88e333619fc8e94b4d69a2a706c
reymesson1 <reymesson1>
2021-03-02 19:28:01 -0400

    done

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 class App extends Component {
   constructor(props) {
@@ -14,12 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
