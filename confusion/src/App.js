29cd2ba7ea94e50bb5803a3523df6fa3aa116cd1
reymesson1 <reymesson1>
2021-03-03 09:29:09 -0400

    Week 3 Redux

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -6,6 +6,10 @@ import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
   constructor(props) {
@@ -16,12 +20,13 @@ class App extends Component {
   }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
-
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
